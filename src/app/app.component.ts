8d856d46a459a983117faae64f8a8a7b53b43869
monicaegm <monicaegm4@gmail.com>
2023-05-05 09:37:01 -0600

    se agrega la logica de la calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  titulo = 'Calculadora';
+  operadorA:number = 0;
+  operadorB:number = 0;
+  resultado:number = 0;
+
+  Sumar():void{
+    this.resultado = this.operadorA + this.operadorB;
+  }
+  Restar():void{
+    this.resultado = this.operadorA - this.operadorB;
+  }
+  Multiplicar():void{
+    this.resultado = this.operadorA * this.operadorB;
+  }
+  Dividir():void{
+    this.resultado = this.operadorA / this.operadorB;
+  }
 }
